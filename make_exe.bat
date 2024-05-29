fc9a09cbdd64ee73839096c84f995934818f4e2c
Dan Phan Manh <danpm@fpt.com>
2024-05-29 17:00:41 +0800

    Fixes script filename

--- make_exe.bat
+++ make_exe.bat
@@ -1,3 +1,3 @@
 rem  -F
-pyinstaller --onedir --add-data "C:\Python312\envs\videosubs\Lib\site-packages\whisper\assets\:./whisper/assets/" transcribe_2_subtitles.py
+pyinstaller --onedir --add-data "C:\Python312\envs\videosubs\Lib\site-packages\whisper\assets\:./whisper/assets/" transcribe.py
 

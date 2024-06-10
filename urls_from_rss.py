303441689d1c857c11983589ee9865e296284c5e
Dan Phan Manh <danpm@fpt.com>
2024-06-11 02:02:20 +0800

    Adds more podcasts

--- urls_from_rss.py
+++ urls_from_rss.py
@@ -33,17 +33,27 @@ def extract_titles_and_enclosure_urls_from_xml(url):
         return []
 
 
+# fmt: off
+
 # URL of the XML file
-# url = "https://teatimechinese.libsyn.com/rss" # TeaTime Chinese
-url = "https://feed.firstory.me/rss/user/ckq9bl3vd660p0805d1apvgrd"  # Learn Mandarin in Mandarin with Huimin
+# url, podcast_name = "https://teatimechinese.libsyn.com/rss", "TeaTime Chinese" # TeaTime Chinese
+# url, podcast_name = "https://feed.firstory.me/rss/user/ckq9bl3vd660p0805d1apvgrd", "Huimin Mandarin"  # Learn Mandarin in Mandarin with Huimin
+url, podcast_name = "https://anchor.fm/s/5caa7664/podcast/rss", "Haike Mandarin"  # 還可中文 Haike Mandarin
+# url, podcast_name = "https://feeds.soundcloud.com/users/soundcloud:users:688207190/sounds.rss", "One Call Awaw"  # 打個電話給你 One Call Awaw
+# url, podcast_name = "https://feed.firstory.me/rss/user/cko0g8j5m1toy0984t6s7mqtk","Convo Chinese" # 瞎扯学中文 Convo Chinese
+# fmt: on
+
+
 # Extract titles and enclosure urls
 titles_and_enclosure_urls = extract_titles_and_enclosure_urls_from_xml(url)
 
 # Save titles and enclosure urls to urls.txt
-with open("./downloads/urls.txt", "w", encoding="utf-8") as file:
+# podcast_name = "convo_chinese"
+filename = f"{podcast_name}_urls.txt"
+with open(f"./downloads/{filename}", "w", encoding="utf-8") as file:
     file.write("# URLs to download. Add a tab and then the Filename if needed\n")
     print(f"Found {len(titles_and_enclosure_urls)} urls")
     for title, enclosure_url in titles_and_enclosure_urls:
-        file.write(f"{enclosure_url}\t{title}\n")
+        file.write(f"{enclosure_url}\t{podcast_name} - {title}\n")
 
-print("Titles and enclosure URLs have been saved to urls.txt")
+print(f"Titles and enclosure URLs have been saved to {filename}")
